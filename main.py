883f45799ea09ba740dc3d5017eae4df06a42ced
Blue-Ball <ShkuratovMong@mail.ru>
2021-08-30 21:30:40 +0900

    code formatting

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ def compress_video(video_full_path, output_file_name, videoResolution):
     print('process:' + output_file_name)
     i = ffmpeg.input(video_full_path)
     ffmpeg.output(i, output_file_name,
-                  **{'loglevel':'panic', 'c:v': 'libx264', 'b:v': videoResolution['rate'], 'vf': 'scale='+videoResolution['width']+':'+videoResolution['height']}
+                  **{'loglevel': 'panic', 'c:v': 'libx264', 'b:v': videoResolution['rate'], 'vf': 'scale='+videoResolution['width']+':'+videoResolution['height']}
                   ).overwrite_output().run()
 
 
-videoResolusions = {'1080p': {'width': '1920', 'height': '1080', 'rate':'1.7M'},
-                    '720p': {'width': '1280', 'height': '720', 'rate':'1.1M'},
-                    '480p': {'width': '858', 'height': '480', 'rate':'0.5M'},
-                    '360p': {'width': '480', 'height': '360', 'rate':'0.25M'}}
+videoResolusions = {'1080p': {'width': '1920', 'height': '1080', 'rate': '1.7M'},
+                    '720p': {'width': '1280', 'height': '720', 'rate': '1.1M'},
+                    '480p': {'width': '858', 'height': '480', 'rate': '0.5M'},
+                    '360p': {'width': '480', 'height': '360', 'rate': '0.25M'}}
 
 mypath = os.path.abspath(__file__)
 rootdir = os.path.dirname(mypath)
@@ -30,7 +30,7 @@ for file in onlyfiles:
         outdir = join(rootdir, 'out\\' + file)
         if not os.path.exists(outdir):
             os.makedirs(outdir)
-        
+
         inputFile = join(inputdir, file)
         print('\ninputFile:' + inputFile)
         for rs in videoResolusions:
